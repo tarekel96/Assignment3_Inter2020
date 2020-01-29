010e81f4a8c0f784b84880b37e14a379123a55c1
root <elhaj102@mail.chapman.edu>
2020-01-29 03:30:43 +0000

    finished defining the getter and setter functions and then created two instances of the Pet class in the main file and invoke the class methods to set the properties for the instances and then output the properties to the console

--- main.cpp
+++ main.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
 #include <string>
-#include "pet.h"
+#include "class_pet.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, char ** argv) {
 
+    Pet oreo;
+
+    oreo.SetName("Oreo");
+    oreo.SetType("Dog");
+    oreo.SetAge(12);
+    oreo.SetWeight(92.6);
+
+   /* cout << "inside main" << endl; */
+
+    cout << endl;
+    cout << "Name: " << oreo.GetName() << endl;
+    cout << "Type: " << oreo.GetType() << endl;
+    cout << "Age: " << oreo.GetAge() << " years old" << endl;
+    cout << "Weight: " << oreo.GetWeight() << " lbs." << endl;
+    cout << endl;
+
+    Pet sofia;
+
+    sofia.SetName("Sofia");
+    sofia.SetType("Dog");
+    sofia.SetAge(9);
+    sofia.SetWeight(45.6);
+
+   /* cout << "inside main" << endl; */
+
+    cout << endl;
+    cout << "Name: " << sofia.GetName() << endl;
+    cout << "Type: " << sofia.GetType() << endl;
+    cout << "Age: " << sofia.GetAge() << " years old" << endl;
+    cout << "Weight: " << sofia.GetWeight() << " lbs." << endl;
+    cout << endl;    
 
     return 0;
 }
